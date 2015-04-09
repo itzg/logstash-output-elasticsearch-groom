edb5983d2362b9813e540d2d5c5423a3d991370e
Geoff Bourne <itzgeoff@gmail.com>
2015-04-08 23:14:18 -0500

    Now identifies indices that need grooming

--- lib/logstash/outputs/elasticsearch_groom.rb
+++ lib/logstash/outputs/elasticsearch_groom.rb
@@ -2,6 +2,7 @@
 require 'logstash/outputs/base'
 require 'logstash/namespace'
 require 'logstash-output-elasticsearch_groom_jars'
+require 'java'
 
 class LogStash::Outputs::ElasticsearchGroom < LogStash::Outputs::Base
   config_name "elasticsearch_groom"
@@ -13,6 +14,8 @@ class LogStash::Outputs::ElasticsearchGroom < LogStash::Outputs::Base
   # Specifies if 'open' or 'closed' indices should be considered.
   # Can include/be an event field reference via %{value} substitution
   config :scope, :validate => :string, :default => 'open'
+
+  config :age_cutoff, :validate => :string, :default => '4w'
 
   public
   def register
@@ -29,18 +32,65 @@ class LogStash::Outputs::ElasticsearchGroom < LogStash::Outputs::Base
 
     tsWildcarded = @index.gsub /%{\+.+}/, '*'
     tsWildcarded = event.sprintf tsWildcarded
-    puts "tsWildcarded is #{tsWildcarded}"
 
     resolvedScope = event.sprintf(@scope)
     return unless validOption? 'scope', resolvedScope, ['open','closed','both']
 
-    ts = event.timestamp # of type Logstash::Timestamp
-
     candidates = @esAccess.matching_indices pattern: tsWildcarded, scope: resolvedScope
-    puts "Found #{candidates}"
+    puts "Starting with #{candidates}"
+
+    if (tsBitMatched = @index.match /%{\+(.+)}/)
+      groomByTime(event, candidates, tsBitMatched)
+    else
+      @logger.warn "Only 'index' with a timestamp placeholder is supported. Instead had #{resolvedIndex}"
+    end
 
     return "Event received"
-  end # def event
+  end
+
+  def groomByTime(event, candidates, tsBitMatched)
+    ts = event.timestamp # of type Logstash::Timestamp
+    resolvedCutoff = event.sprintf(@age_cutoff)
+    cutoffMsec = convertCutoff(resolvedCutoff)
+    return unless cutoffMsec
+
+    eventDt = Java::OrgJodaTime::DateTime.new ts.to_i*1000
+    absCutoffDt = eventDt.minus cutoffMsec
+    puts "Cutoff is #{absCutoffDt}"
+
+    dtFormat = Java::OrgJodaTimeFormat::DateTimeFormat.forPattern tsBitMatched[1]
+    resolvedIndex = event.sprintf (tsBitMatched.pre_match + '(.+)' + tsBitMatched.post_match)
+    indexParseRegex = Regexp.new resolvedIndex
+    puts "Index regex is #{indexParseRegex}"
+
+    needsGrooming = candidates.find_all do |i|
+      if (matchData = indexParseRegex.match(i))
+        indexDt = dtFormat.parseDateTime matchData[1]
+        puts "Parsed DateTime of #{i} is #{indexDt}"
+        next indexDt.isBefore absCutoffDt
+      end
+    end
+    puts "Found these to groom #{needsGrooming}"
+
+  end
+
+  # Converts the cutoff expression into a duration in milliseconds
+  def convertCutoff(cutoffStr)
+    matchData = /(\d+)([hdw])/.match(cutoffStr)
+    if matchData
+      value = matchData[1]
+      return value.to_i * 1000 * 3600 * case matchData[2]
+        when 'h' then 1
+        when 'd' then 24
+        when 'w' then 24*7
+      end
+    else
+      @logger.warn("Invalid cutoff of #{cutoffStr}")
+      return nil
+    end
+  end
+
+  # def event
 
   def validOption?(option, givenValue, validValues)
     valid = validValues.member?(givenValue)
